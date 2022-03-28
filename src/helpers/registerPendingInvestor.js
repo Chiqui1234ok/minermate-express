4052898e68633786f9c7966475655d26a72e1f24
Santiago <santiagogimenez@outlook.com.ar>
2022-03-28 11:15:29 -0300

    added comments

--- src/helpers/registerPendingInvestor.js
+++ src/helpers/registerPendingInvestor.js
@@ -3,6 +3,7 @@ const   helpers = {},
 
 helpers.registerPendingInvestor = async function (data) {
     try {
+        // Arm intentionToPay object for future push into array 👇
         let intentionToPay = {
             projectID: data.projectID,
             stocks: data.stocks,
@@ -10,9 +11,11 @@ helpers.registerPendingInvestor = async function (data) {
             amount: data.amount,
             symbol: data.symbol
         };
+        // Find and update pendingInvestor, pushing more intentions to pay *
         let pendingInvestor = await PendingInvestor.findByIdAndUpdate({userID: data.userID}, { 
             $push: {'intentionToPay': intentionToPay} 
         });
+        // * This could return null, so we check and save the pendingInvestor if didn't exists 👇
         if(!pendingInvestor || !pendingInvestor._id) {
             pendingInvestor = new PendingInvestor({
                 userID: data.userID,
