6b2af8b463e8c3bd4a41a27a2599bd9085294d80
Santiago <santiagogimenez@outlook.com.ar>
2022-03-28 11:16:46 -0300

    first commit. code pending

--- src/helpers/registerInvestor.js
+++ src/helpers/registerInvestor.js
@@ -1,8 +1,16 @@
 const   helpers = {},
         Investor = require('../models/Investor');
 
-helpers.registerInvestor = async function () {
-    // PENDING
+helpers.registerInvestor = async function (data) {
+    try {
+        // const Investor = new Investor({
+        //     userID: data.userID,
+        //     suscribedProject
+        // })
+    }
+    catch (err) {
+        
+    }
 }
 
 module.exports = helpers;
